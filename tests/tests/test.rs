5e762c976df4a4a800f9123102297019508448af
yurii.oliinyk <yurii.oliinyk@orderly.network>
2023-02-09 14:28:06 +0200

    Improve AccountId substitution

--- tests/tests/test.rs
+++ tests/tests/test.rs
@@ -36,16 +36,39 @@ async fn integration_test_example() -> anyhow::Result<()> {
             .view_param_account_id_ret_account_id(user.id().clone().to_string().parse().unwrap())
             .await?
             .value,
+        user.id().clone()
+    );
+
+    assert_eq!(
+        contract_template
+            .view_param_vec_tuple_with_account_id(vec![(user.id().clone(), 1)])
+            .await?
+            .value,
         user.id().to_string().parse().unwrap()
     );
 
     assert_eq!(
         contract_template
-            .view_param_vec_account_id_ret_vec_account_id(vec![user
-                .id()
-                .to_string()
-                .parse()
-                .unwrap()])
+            .view_param_arr_tuples_with_account_id([(user.id().clone(), 1)])
+            .await?
+            .value,
+        user.id().to_string().parse().unwrap()
+    );
+
+    assert_eq!(
+        contract_template
+            .view_param_vec_tuple_of_vec_tuples_with_account_id(vec![(
+                vec![(user.id().clone(), 1)],
+                1
+            )])
+            .await?
+            .value,
+        user.id().to_string().parse().unwrap()
+    );
+
+    assert_eq!(
+        contract_template
+            .view_param_vec_account_id_ret_vec_account_id(vec![user.id().clone()])
             .await?
             .value,
         vec![user.id().to_string().parse().unwrap()]
