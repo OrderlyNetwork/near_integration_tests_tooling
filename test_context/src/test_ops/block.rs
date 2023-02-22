cca5843118021d93e6ce932232c0f7de6adf0878
yurii.oliinyk <yurii.oliinyk@orderly.network>
2023-02-22 08:37:54 +0200

    First working version

--- test_context/src/test_ops/block.rs
+++ test_context/src/test_ops/block.rs
@@ -1,7 +1,13 @@
+use std::{io, marker::PhantomData, pin::Pin};
+
 use crate::context::TestContext;
 use async_trait::async_trait;
-use futures::{future::try_join_all, try_join};
-use integration_tests_toolset::statistic::statistic_consumer::Statistic;
+use futures::{
+    future::{join_all, try_join_all, LocalBoxFuture},
+    stream::FuturesUnordered,
+    try_join, Future, StreamExt,
+};
+use integration_tests_toolset::{error::TestError, statistic::statistic_consumer::Statistic};
 
 use super::runnable::Runnable;
 
@@ -99,6 +105,122 @@ where
 
     pub fn add_concurrent_ops<'a>(mut self, ops: &'a [Box<dyn Runnable<T, U, N, M>>]) -> Self {
         self.concurrent.extend_from_slice(ops);
+        self
+    }
+}
+
+///// here is the alternative implementation
+///
+// #[derive(Debug, Clone, Default)]
+
+type Closure<T, U, const N: usize, const M: usize> =
+    Box<dyn Fn(&TestContext<T, U, N, M>) -> LocalBoxFuture<Result<(), TestError>>>;
+// #[derive(Debug)]
+pub struct Block2<T, U, const N: usize, const M: usize>
+where
+    // Box<dyn Runnable<T, U, N, M>>: Clone,
+    // F: Fn(&TestContext<T, U, N, M>) -> LocalBoxFuture<Result<(), TestError>> + Clone,
+    T: Clone + std::fmt::Debug + Send + Sync,
+    // U: std::fmt::Debug + Clone + 'static,
+{
+    // _phantom_t: PhantomData<T>,
+    // _phantom_u: PhantomData<U>,
+    pub chain: Vec<Closure<T, U, N, M>>,
+    pub concurrent: Vec<Closure<T, U, N, M>>,
+}
+
+impl<T, U, const N: usize, const M: usize> Block2<T, U, N, M>
+where
+    // Box<(dyn Runnable<T, U, N, M>)>: Clone,
+    T: Clone + std::fmt::Debug + Send + Sync,
+    // U: std::fmt::Debug + Clone + 'static,
+    // F: Fn(&TestContext<T, U, N, M>) -> LocalBoxFuture<Result<(), TestError>> + Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            chain: vec![],
+            concurrent: vec![],
+            // _phantom_t: PhantomData,
+            // _phantom_u: PhantomData,
+        }
+    }
+}
+
+// impl<
+//         T: Sync + Send + std::fmt::Debug + Clone + 'static,
+//         U: std::fmt::Debug + Clone + 'static,
+//         const N: usize,
+//         const M: usize,
+//     > From<Block2<T, U, N, M>> for Box<dyn Runnable<T, U, N, M>>
+// {
+//     fn from(block: Block2<T, U, N, M>) -> Self {
+//         Box::new(block)
+//     }
+// }
+
+// #[async_trait]
+impl<
+        T: Clone + std::fmt::Debug + Send + Sync + 'static,
+        U,
+        // U: std::fmt::Debug + Clone + 'static,
+        // F: Fn(&TestContext<T, U, N, M>) -> LocalBoxFuture<Result<(), TestError>> + Clone,
+        //  U: std::fmt::Debug + Clone + 'static,
+        const N: usize,
+        const M: usize,
+    > Block2<T, U, N, M>
+{
+    pub async fn run_impl(
+        &self,
+        context: &TestContext<T, U, N, M>,
+    ) -> anyhow::Result<Option<Statistic>> {
+        try_join!(
+            try_join_all(
+                [|| async {
+                    for op in self.chain.iter() {
+                        op(context).await?;
+                    }
+                    Ok(())
+                }]
+                .iter()
+                .map(|a| a())
+            ),
+            try_join_all(self.concurrent.iter().map(|op| op(context)))
+        )?;
+
+        Ok(None)
+    }
+
+    // fn clone_dyn(&self) -> Box<dyn Runnable<T, U, N, M>> {
+    //     Box::new((*self).clone())
+    // }
+}
+
+impl<T, U, const N: usize, const M: usize> Block2<T, U, N, M>
+where
+    // Box<dyn Runnable<T, U, N, M>>: Clone,
+    // F: Fn(&TestContext<T, U, N, M>) -> LocalBoxFuture<Result<(), TestError>> + Clone,
+    // U: std::fmt::Debug + Clone + 'static,
+    T: Clone + std::fmt::Debug + Send + Sync,
+{
+    pub fn add_chain_op(mut self, op: Closure<T, U, N, M>) -> Self {
+        self.chain.push(op);
+        self
+    }
+
+    pub fn add_chain_ops(mut self, ops: Vec<Closure<T, U, N, M>>) -> Self {
+        self.chain.extend(ops.into_iter());
+        // self.chain.extend_from_slice(ops);
+        self
+    }
+
+    pub fn add_concurrent_op(mut self, op: Closure<T, U, N, M>) -> Self {
+        self.concurrent.push(op);
+        self
+    }
+
+    pub fn add_concurrent_ops(mut self, ops: Vec<Closure<T, U, N, M>>) -> Self {
+        // self.concurrent.extend_from_slice(ops);
+        self.chain.extend(ops.into_iter());
         self
     }
 }
